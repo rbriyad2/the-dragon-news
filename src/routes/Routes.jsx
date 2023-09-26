616cddd5ece147f41369fcc6d06b90c1dae0870b
Riyad Ahmed <rbriyad2@gmail.com>
2023-09-26 15:20:18 +0600

    git depoloy

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -40,7 +40,7 @@ const router = createBrowserRouter([
         {
           path: ":id",
           element: <Category></Category>,
-          loader: ({params})=> fetch(`http://localhost:5000/categories/${params.id}`)
+          loader: ({params})=> fetch(`https://the-dragon-news-server-3frrum7h7-rbriyad2gmailcoms-projects.vercel.app/categories/${params.id}`)
         }
       ]
     },
@@ -51,7 +51,7 @@ const router = createBrowserRouter([
         {
           path: ':id',
           element: <PrivateRoute><News></News></PrivateRoute>,
-          loader: ({params})=> fetch(`http://localhost:5000/news/${params.id}`)
+          loader: ({params})=> fetch(`https://the-dragon-news-server-3frrum7h7-rbriyad2gmailcoms-projects.vercel.app/news/${params.id}`)
         }
         
       ]
